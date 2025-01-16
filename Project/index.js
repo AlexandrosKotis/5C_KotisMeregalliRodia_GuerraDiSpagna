a0ecea144e6df96e2936d9f5d119f03050258495
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-01-16 17:05:22 +0100

    change things

--- Project/index.js
+++ Project/index.js
@@ -1,8 +1,3 @@
-
-
-
 import { createNavigator } from "./scripts/navigator.js";
-import { createMap } from "./scripts/createMap.js.js";
 
-const navigator = createNavigator(document.querySelector("#container"));
-const map = createMap(document.querySelector("#map"));
+const navigator = createNavigator(document.querySelector("#pages"));
