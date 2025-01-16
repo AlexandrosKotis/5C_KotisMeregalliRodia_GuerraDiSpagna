e30ff734d900c880617e8e7976c6a013173ba1cb
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2025-01-16 12:50:49 +0100

    mappa

--- Project/index.js
+++ Project/index.js
@@ -2,5 +2,7 @@
 
 
 import { createNavigator } from "./scripts/navigator.js";
+import { createMap } from "./scripts/createMap.js.js";
 
 const navigator = createNavigator(document.querySelector("#container"));
+const map = createMap(document.querySelector("#map"));
