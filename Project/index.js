c833722b41620bf377950558ae23aaf4e1d23279
RodiaThomas <rodiathomas@itis-molinari.eu>
2025-01-17 20:09:56 +0100

    form e login Rodia

--- Project/index.js
+++ Project/index.js
@@ -6,11 +6,11 @@ import { createForm } from "./scripts/createForm.js";
 
 //Binding
 const navigator = createNavigator(document.querySelector("#pages"));
-const formElement = document.querySelector("#form");
+
 const mapElement = document.querySelector("#map");
 const tableElement = document.querySelector("#table");
 
-const form = createForm(formElement);
+
 const map = createMap(mapElement);
 const table = createTable(tableElement);
 
@@ -21,3 +21,36 @@ fetchCache.build("/config.json", "cache").then(() => {
         });
     })
 }).catch(console.error);
+
+const formElement = document.querySelector("#form"); // prendi il div form 
+const form = createForm(formElement); // crea l'elemento form
+let dati=["titolo","descrizione","anni","longitudine","latitudine","contendenti","morti","vincitore","foto"];//dati da inserire nella form x kotis se riesci a prenderli dal config sarebbe meglio
+form.setLabels(dati);// inseriscili nella form
+fin={};//dizionario dove troveremo i valori della form dopo aver premuto submit ATTENZIONE! Ricordarsi di pulirlo dopo aver caricato i valori nella cache 
+finlogin={}//dizionario dove troveremo i valori della login dopo aver premuto submit ATTENZIONE! Ricordarsi di pulirlo dopo cpntrollato l'accesso 
+function dizzForm(res){// funzione che popolerà il dizionario finale 
+   
+    for(let i=0;i<dati.length;i++){
+        fin[dati[i]]=res[i]
+    }
+    fin["anni"]=fin["anni"].split("-");
+    fin["coordinate"]=[fin["longitudine"],fin["latitudine"]];
+    fin["contendenti"]=fin["contendenti"].split("-");
+    fin["foto"]=fin["foto"].split("-");
+
+
+}
+form.onsubmit(dizzForm);// carichiamo la funzione all onclik 
+form.render();// renderiziamo
+
+const loginElement = document.querySelector("#login");// prendi il div inerente 
+const login = createForm(loginElement);//crea il log in
+let datilogin=["username","password"];// dati log in x kotis se riesci a prenderli dal config sarebbe meglio
+login.setLabels(datilogin);// inerisci i dati
+function dizzLogin(res){// funzione che popolerà il dizionario finale login
+    for(let i=0;i<datilogin.length;i++){
+        finlogin[datilogin[i]]=res[i];
+    }
+}
+login.onsubmit(dizzLogin);// carichiamo la funzione all onclik 
+login.render();//renderiziamo
