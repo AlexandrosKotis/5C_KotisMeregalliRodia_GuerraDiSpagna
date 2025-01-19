1f08d13fcf028ac23e9acdb7c9f18b6b31ba883f
Thomas Rodia <rodiathomas@itis-molinari.eu>
2025-01-19 14:27:23 +0100

    Tabella fatta da meregali per tet git malfunzionante

--- Project/scripts/createTable.js
+++ Project/scripts/createTable.js
@@ -1,167 +1,143 @@
-import { generateFetchComponent } from "./fetchCache.js";
+import { generateFetchComponent } from "./fetch.js";
 
 export const createTable = (parentElement) => {
     let fetchComp;
     let searchCallback = null;
-
+    let callback=null; 
     return {
-        render: () => {
+         onsubmit:(callbackinput)=>{
+            callback=callbackinput;
+         },
+        render: (newList) => {
+            if(newList===null || newList===undefined || newList===""){
             return new Promise((resolve, reject) => {
-                return fetchComp.getPostData().then((d) => {
-                    let data = JSON.parse(d);
-                    let listToShow = data;
-                    let html = `
-                <div class="container shadow p-4 rounded">
-    <div class="d-flex flex-column flex-sm-row align-items-center justify-content-between mb-4">
-        <div></div>
-        <label for="table-search" class="visually-hidden">Search</label>
-        <div class="position-relative">
-            <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search text-secondary" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85a1.007 1.007 0 0 0-.115-.098zm-5.42.656a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
-                </svg>
-            </div>
-            <input type="text" id="table-search" class="form-control ps-5" placeholder="Search for items">
-            <button id="search-table" type="button" class="btn btn-primary position-absolute top-50 end-0 translate-middle-y">
-                Search
-            </button>
-        </div>
-    </div>
+                fetchComp
+                    .getPostData()
+                    .then((data) => {
+                        const listToShow = JSON.parse(data);
 
-    <div id="tab">
-        <table class="table table-striped table-hover">
-            <thead class="table-light sticky-top">
-                <tr>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col">Targhe</th>
-                    <th scope="col">Morti</th>
-                    <th scope="col">Feriti</th>
-                    <th scope="col">Data/Ora</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Popolato dinamicamente -->
-                <script>
-                    let html = "";
-                    for (const element in listToShow) {
-                        html += 
-                        <tr>
-                            <td>${listToShow[element].address.display_name}</td>
-                            <td>${listToShow[element].targhe}</td>
-                            <td>${listToShow[element].morti}</td>
-                            <td>${listToShow[element].feriti}</td>
-                            <td>${new Date(listToShow[element].dataora).toUTCString()}</td>
-                        </tr>;
-                    }
-                    document.write(html);
-                </script>
-            </tbody>
-        </table>
-    </div>
-</div>
-                `;
-                    parentElement.innerHTML = html;
-                    document.getElementById("search-table").onclick = () => searchCallback(document.getElementById("table-search"));
-                    return resolve(html);
-                }).catch(reject);
-            });
-        },
-        renderFiltered: (filtered) => {
-            return new Promise((resolve, reject) => {
-                return fetchComp.getPostData().then((d) => {
-                    filtered = filtered === " " ? "Milano" : filtered;
-                    let data = JSON.parse(d);
-                    let listToShow = data;
-                    let html = `
-                <div class="container shadow p-4 rounded">
-    <div class="d-flex flex-column flex-sm-row align-items-center justify-content-between mb-4">
-        <div></div>
-        <label for="table-search" class="visually-hidden">Search</label>
-        <div class="position-relative">
-            <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search text-secondary" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85a1.007 1.007 0 0 0-.115-.098zm-5.42.656a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
-                </svg>
-            </div>
-            <input type="text" id="table-search" class="form-control ps-5" placeholder="Search for items">
-            <button id="search-table" type="button" class="btn btn-primary position-absolute top-50 end-0 translate-middle-y">
-                Search
-            </button>
-        </div>
-    </div>
+                        const html = `
+                        <div class="container shadow p-4 rounded">
+                            <div class="d-flex justify-content-end mb-3">
+                                <input
+                                    type="text"
+                                    id="table-search"
+                                    class="form-control w-50"
+                                    placeholder="Cerca un evento"
+                                />
+                                <button id="search-table" class="btn btn-primary ms-2">Cerca</button>
+                            </div>
+                            <table class="table table-striped table-hover text-center">
+                                <thead class="table-light sticky-top">
+                                    <tr>
+                                        <th scope="col">Evento</th>
+                                        <th scope="col">Dettagli</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="table-body">
+                                    ${listToShow
+                                        .map(
+                                            (item) => `
+                                            <tr>
+                                                <td>${item.evento}</td>
+                                                <td>
+                                                    <button class="btn btn-primary">${item.dettagli}</button>
+                                                </td>
+                                            </tr>`
+                                        )
+                                        .join("")}
+                                </tbody>
+                            </table>
+                        </div>`;
 
-    <div id="tab">
-        <table class="table table-striped table-hover text-center">
-            <thead class="table-light sticky-top">
-                <tr>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col">Targhe</th>
-                    <th scope="col">Morti</th>
-                    <th scope="col">Feriti</th>
-                    <th scope="col">Data/Ora</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Contenuto dinamico -->
-                <script>
-                    let html = "";
-                    for (const element in listToShow) {
-                        html += 
-                        <tr>
-                            <td>${listToShow[element].address.display_name}</td>
-                            <td>${listToShow[element].targhe}</td>
-                            <td>${listToShow[element].morti}</td>
-                            <td>${listToShow[element].feriti}</td>
-                            <td>${new Date(listToShow[element].dataora).toUTCString()}</td>
-                        </tr>;
-                    }
-                    document.write(html);
-                </script>
-            </tbody>
-        </table>
-    </div>
-</div>`;
-                    for (const element in listToShow) {
-                        if (((listToShow[element].address.display_name).toLowerCase()).includes((filtered.toLowerCase()))) {
-                            html += `
-                                <tr class="table-light border-bottom">
-    <th scope="row" class="px-4 py-3 fw-semibold text-dark text-nowrap">
-        ${listToShow[element].address.display_name}
-    </th>
-    <td class="px-4 py-3">
-        ${listToShow[element].targhe}
-    </td>
-    <td class="px-4 py-3">
-        ${listToShow[element].morti}
-    </td>
-    <td class="px-4 py-3">
-        ${listToShow[element].feriti}
-    </td>
-    <td class="px-4 py-3">
-        ${new Date(listToShow[element].dataora).toUTCString()}
-    </td>
-</tr>`;
-                        }
-                    }
-                    html += `
-    </tbody>
-</table>
-</div>
-</div>`;
-                    parentElement.innerHTML = html;
-                    document.getElementById("search-table").onclick = () => searchCallback(document.getElementById("table-search"));
-                    return resolve(html);
-                }).catch(reject);
+                        parentElement.innerHTML = html;
+
+                        const searchInput = document.getElementById("table-search");
+                        const searchButton = document.getElementById("search-table");
+
+                        searchButton.onclick = () => {
+
+                          //  console.info(this.searchCallback);
+                           // if (searchCallback) {
+                           const result=searchInput.value;
+                            callback(result);
+                            searchInput.value="";
+                           // }
+                        };
+
+                        resolve();
+                    })
+                    .catch(reject);
             });
+        }else{
+
+          const listToShow = newList;
+
+            const html = `
+            <div class="container shadow p-4 rounded">
+                <div class="d-flex justify-content-end mb-3">
+                    <input
+                        type="text"
+                        id="table-search"
+                        class="form-control w-50"
+                        placeholder="Cerca un evento"
+                    />
+                    <button id="search-table" class="btn btn-primary ms-2">Cerca</button>
+                </div>
+                <table class="table table-striped table-hover text-center">
+                    <thead class="table-light sticky-top">
+                        <tr>
+                            <th scope="col">Evento</th>
+                            <th scope="col">Dettagli</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body">
+                        ${listToShow
+                            .map(
+                                (item) => `
+                                <tr>
+                                    <td>${item.evento}</td>
+                                    <td>
+                                        <button class="btn btn-primary">${item.dettagli}</button>
+                                    </td>
+                                </tr>`
+                            )
+                            .join("")}
+                    </tbody>
+                </table>
+            </div>`;
+        
+            parentElement.innerHTML = html;
+        
+            const searchInput = document.getElementById("table-search");
+            const searchButton = document.getElementById("search-table");
+        
+            searchButton.onclick = () => {
+        
+              //  console.info(this.searchCallback);
+               // if (searchCallback) {
+               const result=searchInput.value;
+                callback(result);
+                searchInput.value="";
+               // }
+            };
+        }
         },
+
+
         build: () => {
             return new Promise((resolve, reject) => {
-                feetchComp = gnerateFetchComponent();
-                fetchComp.build("../../config.json", "cache").then(resolve).catch(reject);
+                fetchComp = generateFetchComponent();
+                fetchComp.build().then(resolve).catch(reject);
             });
         },
-        searchCallback: (callback) => {
-            searchCallback = callback;
-        }
+
+       
     };
 };
+
+
+
+
+   
+
